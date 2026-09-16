311fbd91ecbf198a63103e6cc42b415b4bd51daa
AleT-23 <00128925@uca.edu.sv>
2026-09-16 01:33:39 -0600

    feat: agregar ruta de eliminar clasificacion automatica

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //Importar funciones
-const { registrarIncidencia, listarIncidencias, buscarIncidencias, cambiarEstadoIncidencia, eliminarIncidencia } = require("../controllers/incidenciasController");
+const { registrarIncidencia, listarIncidencias, buscarIncidencias, cambiarEstadoIncidencia, eliminarIncidencia,obtenerEstadisticas,clasificarIncidencia} = require("../controllers/incidenciasController");
 
 // funcion registrar incidencias
 router.post("/", registrarIncidencia);
@@ -10,8 +10,14 @@ router.post("/", registrarIncidencia);
 // Funcion de mostrar todas las incidencias 
 router.get("/", listarIncidencias);
 
+// funcion de obtener estadisticas
+router.get("/estadisticas", obtenerEstadisticas);
+
 //Funcion buscar incidencias dado su id
 router.get("/:id", buscarIncidencias);
+
+// funcion de clasificacion por incidencia 
+router.get("/:id/clasificacion", clasificarIncidencia);
 
 //Funcion cambiar estado de una incidencia
 router.put("/:id/estado", cambiarEstadoIncidencia);
