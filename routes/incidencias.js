13be86894a8e1806572b8b2a4c5f370ed77041e8
AleT-23 <00128925@uca.edu.sv>
2026-09-17 11:09:17 -0600

    fix: corregir orden de rutas de incidencias

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //Importar funciones
-const { registrarIncidencia, listarIncidencias, buscarIncidencias, cambiarEstadoIncidencia, eliminarIncidencia,obtenerEstadisticas,clasificarIncidencia} = require("../controllers/incidenciasController");
+const { registrarIncidencia, listarIncidencias, buscarIncidencias, cambiarEstadoIncidencia, eliminarIncidencia, obtenerEstadisticas, clasificarIncidencia } = require("../controllers/incidenciasController");
 
 // funcion registrar incidencias
 router.post("/", registrarIncidencia);
@@ -13,11 +13,11 @@ router.get("/", listarIncidencias);
 // funcion de obtener estadisticas
 router.get("/estadisticas", obtenerEstadisticas);
 
-//Funcion buscar incidencias dado su id
-router.get("/:id", buscarIncidencias);
-
 // funcion de clasificacion por incidencia 
 router.get("/:id/clasificacion", clasificarIncidencia);
+
+//Funcion buscar incidencias dado su id
+router.get("/:id", buscarIncidencias);
 
 //Funcion cambiar estado de una incidencia
 router.put("/:id/estado", cambiarEstadoIncidencia);
